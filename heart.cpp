2f2647a660a656ebf7adf5fd9920aea6334aa279
darkstar250721-pixel <darkstar.250721@gmail.com>
2026-09-09 16:15:42 +0700

    heart.cpp

---
+++ heart.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    for (float y = 1.5; y > -1.5; y -= 0.1) {
+        for (float x = -1.5; x < 1.5; x += 0.05) {
+            float a = x*x + y*y - 1;
+            if (a*a*a - x*x*y*y*y <= 0.0)
+                cout << "*";
+            else
+                cout << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
